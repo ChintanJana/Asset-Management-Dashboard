61064c801fb35ce475fc1e9cf3469ea6869aac16
ChintanJana <117261030+ChintanJana@users.noreply.github.com>
2024-03-12 18:33:30 +0530

    Create __init__.py

---
+++ backend/routers/__init__.py
@@ -0,0 +1,11 @@
+# Import statements
+from .asset_router import asset
+from .metric_router import metric
+from .aggregation_router import aggregation
+
+# Exported symbols (optional)
+__all__ = [
+    "asset",
+    "metric"
+    "aggregation"
+]
